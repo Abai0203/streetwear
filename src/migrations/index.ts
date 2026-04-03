2fa11406e4bee8790382207f5ac51644beb15f83
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-04-03 15:55:40 +0600

    add role to users

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20260322_233106_initial from './20260322_233106_initial'
+import * as migration_20260322_233106_initial from './20260322_233106_initial';
+import * as migration_20260403_095525_add_role_to_users from './20260403_095525_add_role_to_users';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20260322_233106_initial.down,
     name: '20260322_233106_initial',
   },
-]
+  {
+    up: migration_20260403_095525_add_role_to_users.up,
+    down: migration_20260403_095525_add_role_to_users.down,
+    name: '20260403_095525_add_role_to_users'
+  },
+];
