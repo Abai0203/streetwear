25a2b0debef6c2fb73a94b5af33e9aaef136d184
Abai0203 <abaizylkybaevaba7@gmail.com>
2026-04-24 17:03:48 +0600

    update collections media

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,25 @@
 import type { CollectionConfig } from 'payload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
+    create: ({ req: { user } }) => !!user,
+    update: ({ req: { user } }) => !!user,
+    delete: ({ req: { user } }) => !!user,
   },
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: true,
-    },
-  ],
-  upload: true,
+  upload: {
+    staticDir: path.resolve(dirname, '../../public/media'),
+    imageSizes: [
+      { name: 'thumbnail', width: 400, height: 400, position: 'centre' },
+      { name: 'card', width: 800, height: 800, position: 'centre' },
+    ],
+    adminThumbnail: 'thumbnail',
+    mimeTypes: ['image/*'],
+  },
+  fields: [{ name: 'alt', label: 'Alt-текст', type: 'text' }],
 }
